14247616cf10e61b3252e90b16fd43cd95da66ed
David Shrader <dshrader38@yahoo.com>
2014-11-24 23:30:17 -0600

    Update gcc.sh

--- gcc_build/gcc.sh
+++ gcc_build/gcc.sh
@@ -48,3 +48,16 @@ make install
 # Uncomment for cleanup …
 cd ../../..
 # rm -rf temp-gcc-$VERSION
+
+#Libraries have been installed in:
+#   /usr/local/gcc-4.9.2/lib
+
+#If you ever happen to want to link against installed libraries
+#in a given directory, LIBDIR, you must either use libtool, and
+#specify the full pathname of the library, or use the `-LLIBDIR'
+#flag during linking and do at least one of the following:
+#   - add LIBDIR to the `DYLD_LIBRARY_PATH' environment variable
+#     during execution
+
+#See any operating system documentation about shared libraries for
+#more information, such as the ld(1) and ld.so(8) manual pages.
